aa60b32129d1544aaa6fe3e2e2323c877cbfba3c
Jesse Karnavaara <jesse.karnavaara@edu.lapinamk.fi>
2021-03-22 18:12:24 +0200

    Fix: Added a heroku connection URI

--- src/db/chatter_app_api.js
+++ src/db/chatter_app_api.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 
-const url = "mongodb://127.0.0.1:27017/chatter_app_api";
+// const url = "mongodb://127.0.0.1:27017/chatter_app_api";
+const url =
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/chatter_app_api";
 
 const connection = async () => {
   try {
